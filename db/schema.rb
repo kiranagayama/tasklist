ba735eaad59446d60406e4b4d53965c282c79dfd
kira.nagayama <kira20011025@gmail.com>
2022-08-30 13:13:52 +0000

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_060237) do
+ActiveRecord::Schema.define(version: 2022_08_30_130256) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
